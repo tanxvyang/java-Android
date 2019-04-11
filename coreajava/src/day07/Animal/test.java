1565fc6cd4f59338f361058470071ae5a6404123
谭旭洋 <tanxvyang@outlook.com>
2019-04-11 15:13:18 +0800

    1

--- coreajava/src/day07/Animal/test.java
+++ coreajava/src/day07/Animal/test.java
@@ -1,16 +1,14 @@
 package day07.Animal;
 
-import sun.corba.Bridge;
-
 public class test {
     public static void main(String[] args) {
-        /*
-        * 1.堆中分配父类的空间
-        * 2.堆中分配子类空间
-        * 3.属性初始化
-        * 4.调用构造方法,其中构造方法的第一行是调用父类的构造方法
-        *
-        * */
+        // /*
+        // * 1.堆中分配父类的空间
+        // * 2.堆中分配子类空间
+        // * 3.属性初始化
+        // * 4.调用构造方法,其中构造方法的第一行是调用父类的构造方法
+        // *
+        // * */
 
 
 
@@ -22,6 +20,8 @@ public class test {
 
         Dog d=new Dog("jingba");
         System.out.println(d.getName());
+
+        System.out.println("--------------------------------");
         d.eat();
     }
 }
